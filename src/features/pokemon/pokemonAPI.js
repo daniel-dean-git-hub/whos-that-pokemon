6fc80b976c03a3f3fc94039e7a3bef83f7c57941
DanDean191 <Daniel.Dean191@hotmail.com>
2022-06-05 14:16:38 +0100

    Deployed to github pages

--- src/features/pokemon/pokemonAPI.js
+++ src/features/pokemon/pokemonAPI.js
@@ -9,11 +9,10 @@ export const fetchPokemonDetails = async (currentId) => {
         newID = newNumber
     }
 
-    const test = await fetch(`https://pokeapi.co/api/v2/pokemon/${newID}`)
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${newID}`)
     .then(response => response.json())
     
-    console.log(test)
+    // console.log(pokemon)
 
-    return test
-
+    return pokemon
 }

831f6cdef3f346ea6bfa702f32eb8d1349d4e8f1
DanDean191 <Daniel.Dean191@hotmail.com>
2022-08-20 23:21:47 +0100

    fixed loading issue

--- src/features/pokemon/PokemonContainer.js
+++ src/features/pokemon/PokemonContainer.js
@@ -3,42 +3,37 @@ import './Pokemon.scss'
 import { useSelector, useDispatch } from 'react-redux'
 import { getPokemon, selectPokemon, selectLoading, selectVisibility, imageLoaded } from './pokemonSlice'
 import pokeball from '../../assets/images/pokeball.png'
-
-/** @jsxImportSource @emotion/react */
-// import { css } from "@emotion/react";
-
 import Pokemon from './Pokemon'
 
 const PokemonContainer = () => {
     const dispatch = useDispatch()    
     const pokemon = useSelector(selectPokemon)
-    const loading = useSelector(selectLoading)
+    const pokemonLoading = useSelector(selectLoading)
     const visibility = useSelector(selectVisibility)
     
     useEffect(() => {
       dispatch(getPokemon())
     }, [dispatch])
     
-
     const loaded = () => dispatch(imageLoaded())
-    const loadingClass = loading ? ' img-loading' : ''
+    const loadingClass = pokemonLoading ? ' img-loading' : ''
     const pokemonVisibility = visibility ? '' : 'hidden' 
     const styleClasses = `${loadingClass} ${pokemonVisibility}`
 
     return (
-        <div css={{
-            display: 'flex',
-            background: '#ceebe6',
-            border: '2rem solid hsl(188, 53%, 49%)', //
-            margin: 'auto',
-            maxWidth: 1200,
-            padding: '3rem',
-            borderRadius: '50%',
-          }}>
-            {/* <div className="pokemon-container"> */}
-                { loading && <img className='pokemon loading' draggable={false} src={pokeball} alt='loading' /> } 
+        <div className='pokemon-container'>
+                { pokemonLoading && 
+                  <img 
+                    height='475'
+                    width='475'
+                    loading='lazy'
+                    className='pokemon loading' 
+                    draggable={false} 
+                    src={pokeball} 
+                    alt='loading'
+                  />        
+                } 
                 { Object.keys(pokemon).length !== 0 && <Pokemon styleClasses={styleClasses} pokemonDetails={pokemon} changeLoadState={loaded}/>}
-            {/* </div> */}
           </div>
     )
 }
